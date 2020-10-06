fb9fea1d030958c6266455a1fe1a49ff980080de
黃品皓(pinghao huang) <Z00047010@ctcb.cfhc.com>
2020-10-06 17:11:27 +0800

    add cmd bat

---
+++ cmd.bat
@@ -0,0 +1,12 @@
+SET JAVA_HOME=D:\paul\tool\jdk\jdk8_64
+SET MAVEN_HOME=D:\paul\tool\maven\apache-maven-3.6.3
+SET GIT_HOME=D:\paul\tool\git\PortableGit
+SET NODEJS_HOME=D:\paul\tool\nodejs\node-v12.18.4-win-x64
+SET PYTHON_HOME=C:\Users\Z00047010\AppData\Local\Programs\Python\Python38-32
+
+SET PATH=%PATH%;%JAVA_HOME%\bin;%MAVEN_HOME%\bin;%GIT_HOME%\bin;%NODEJS_HOME%;%PYTHON_HOME%;%PYTHON_HOME%\Scripts;
+
+cd D:\paul\code\projects
+D:
+cmd
+
